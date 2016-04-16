748154df3bc6d29a46edc7491d49ff6f8cac4894
Oleg Sushkov <osushkov@gmail.com>
2016-04-16 14:09:46 +1000

    a little bit of cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,8 @@
 #include <string>
 #include <vector>
 
+#include "DataLoader.hpp"
 #include "common/Common.hpp"
-#include "image/IdxImages.hpp"
-#include "image/IdxLabels.hpp"
-#include "image/ImageGenerator.hpp"
-#include "image/ImageWriter.hpp"
 #include "learningpolicies/DynamicTrainer.hpp"
 #include "learningpolicies/DynamicTrainerBuilder.hpp"
 #include "learningpolicies/SimpleTrainer.hpp"
@@ -28,104 +25,6 @@
 
 using namespace std;
 using Eigen::MatrixXd;
-
-// Number of images to generate using rotation and translation from each canonical training image.
-static constexpr unsigned NUM_DERIVED_IMAGES = 5;
-
-static constexpr float GENERATED_IMAGE_SHIFT_X = 0.1f;
-static constexpr float GENERATED_IMAGE_SHIFT_Y = 0.1f;
-static constexpr float GENERATED_IMAGE_ROT_THETA = 10.0f * M_PI / 180.0f;
-static constexpr float PIXEL_DROPOUT_RATE = 0.1f;
-
-static const ImageGenerator imageGenerator(GENERATED_IMAGE_SHIFT_X, GENERATED_IMAGE_SHIFT_Y,
-                                           GENERATED_IMAGE_ROT_THETA, PIXEL_DROPOUT_RATE);
-
-map<int, vector<CharImage>> loadLabeledImages(string imagePath, string labelPath) {
-  IdxImages imageLoader(imagePath);
-  IdxLabels labelLoader(labelPath);
-
-  vector<int> labels = labelLoader.Load();
-  vector<CharImage> images = imageLoader.Load();
-
-  assert(labels.size() == images.size());
-
-  map<int, vector<CharImage>> result;
-  for (unsigned i = 0; i < labels.size(); i++) {
-    if (result.find(labels[i]) == result.end()) {
-      result[labels[i]] = vector<CharImage>();
-    }
-
-    result[labels[i]].push_back(images[i]);
-  }
-
-  return result;
-}
-
-map<int, vector<CharImage>> generateDerivedImages(const map<int, vector<CharImage>> &labeledImages,
-                                                  string outDirectory, unsigned numDerived) {
-
-  assert(numDerived >= 1);
-  map<int, vector<CharImage>> result;
-
-  for (const auto &entry : labeledImages) {
-    int digit = entry.first;
-
-    result[digit] = vector<CharImage>();
-    result[digit].reserve(entry.second.size() * numDerived);
-
-    for (const auto &image : entry.second) {
-      vector<CharImage> generated = imageGenerator.GenerateImages(image, numDerived);
-
-      for (auto &gimage : generated) {
-        assert(gimage.width == image.width && gimage.height == image.height);
-        result[digit].push_back(gimage);
-      }
-    }
-  }
-
-  return result;
-}
-
-TrainingSample sampleFromCharImage(int label, const CharImage &img) {
-  Vector output(10);
-  output.fill(0.0f);
-  output[label] = 1.0f;
-
-  Vector input(img.pixels.size());
-  for (unsigned i = 0; i < img.pixels.size(); i++) {
-    input(i) = img.pixels[i];
-  }
-
-  return TrainingSample(input, output);
-}
-
-vector<TrainingSample> loadSamples(string inImagePath, string inLabelPath, bool genDerived) {
-  auto labeledImages = loadLabeledImages(inImagePath, inLabelPath);
-
-  if (genDerived) {
-    labeledImages = generateDerivedImages(labeledImages, "data/images/", NUM_DERIVED_IMAGES);
-  }
-
-  vector<TrainingSample> result;
-  int inputSize = 0;
-  int outputSize = 0;
-
-  for (const auto &entry : labeledImages) {
-    for (const auto &image : entry.second) {
-      result.push_back(sampleFromCharImage(entry.first, image));
-      inputSize = result.back().input.rows();
-      outputSize = result.back().expectedOutput.rows();
-    }
-  }
-
-  for (const auto &sample : result) {
-    assert(inputSize > 0 && outputSize > 0);
-    assert(sample.input.rows() == inputSize);
-    assert(sample.expectedOutput.rows() == outputSize);
-  }
-
-  return result;
-}
 
 int digitFromNNOutput(const Vector &out) {
   assert(out.rows() == 10);
@@ -143,18 +42,16 @@ int digitFromNNOutput(const Vector &out) {
   return result;
 }
 
-float testNetwork(Network &network, const std::vector<TrainingSample> &evalSamples) {
+float eval(Network &network, const vector<TrainingSample> &testSamples) {
   unsigned numCorrect = 0;
 
-  for (const auto &es : evalSamples) {
-    auto result = network.Process(es.input);
-    // cout << es << " -> " << result << endl << endl;
-
-    bool isCorrect = digitFromNNOutput(result) == digitFromNNOutput(es.expectedOutput);
+  for (const auto &ts : testSamples) {
+    auto result = network.Process(ts.input);
+    bool isCorrect = digitFromNNOutput(result) == digitFromNNOutput(ts.expectedOutput);
     numCorrect += isCorrect ? 1 : 0;
   }
 
-  return 1.0f - (numCorrect / static_cast<float>(evalSamples.size()));
+  return 1.0f - numCorrect / static_cast<float>(testSamples.size());
 }
 
 Network createNewNetwork(unsigned inputSize, unsigned outputSize) {
@@ -242,7 +139,7 @@ void learn(Network &network, vector<TrainingSample> &trainingSamples,
   trainer->AddProgressCallback(
       [&trainingSamples, &testSamples](Network &network, float trainError, unsigned iter) {
         if (iter % 100 == 0) {
-          float testWrong = testNetwork(network, testSamples);
+          float testWrong = eval(network, testSamples);
           cout << iter << "\t" << testWrong << " " << trainError << endl;
           // cout << iter << "\t" << trainError << "\t" << testWrong << endl;
 
@@ -256,21 +153,6 @@ void learn(Network &network, vector<TrainingSample> &trainingSamples,
   // AllowSelectLayers restrictLayers({1});
   trainer->Train(network, trainingSamples, 20000, nullptr);
   cout << "finished" << endl;
-}
-
-void eval(Network &network, const vector<TrainingSample> &testSamples) {
-  unsigned numCorrect = 0;
-
-  for (const auto &ts : testSamples) {
-    auto result = network.Process(ts.input);
-    bool isCorrect = digitFromNNOutput(result) == digitFromNNOutput(ts.expectedOutput);
-    numCorrect += isCorrect ? 1 : 0;
-  }
-
-  cout << endl
-       << "percent correct: " << (100.0f * (numCorrect / static_cast<float>(testSamples.size())))
-       << endl;
-  ;
 }
 
 void testAutoencoder(const vector<TrainingSample> &digitSamples) {
@@ -295,13 +177,13 @@ int main(int argc, char **argv) {
 
   cout << "loading training data" << endl;
   vector<TrainingSample> trainingSamples =
-      loadSamples("data/train_images.idx3", "data/train_labels.idx1", true);
+      DataLoader::loadSamples("data/train_images.idx3", "data/train_labels.idx1", true);
   random_shuffle(trainingSamples.begin(), trainingSamples.end());
   cout << "training data size: " << trainingSamples.size() << endl;
 
   cout << "loading test data" << endl;
   vector<TrainingSample> testSamples =
-      loadSamples("data/test_images.idx3", "data/test_labels.idx1", false);
+      DataLoader::loadSamples("data/test_images.idx3", "data/test_labels.idx1", false);
   random_shuffle(testSamples.begin(), testSamples.end());
   cout << "test data size: " << testSamples.size() << endl;
 
@@ -319,7 +201,8 @@ int main(int argc, char **argv) {
     network.Serialize(networkOut);
   } else if (argc == 3 && string(argv[1]) == "eval") {
     Network network = loadNetwork(argv[2]);
-    eval(network, testSamples);
+    float errorRate = eval(network, testSamples);
+    cout << endl << "error rate: " << (100.0f * (1.0f - errorRate)) << endl;
   } else {
     cout << "invalid arguments, expected: " << endl;
     cout << string(argv[0]) << " train [existing_network_file]" << endl;
